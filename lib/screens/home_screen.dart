034193143c2456fc02bf39438842f750649b7641
layton <laytonmatheka@gmail.com>
2024-02-12 10:53:40 +0300

    home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -15,35 +15,178 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Home Page"),
+        title: const Text("Home Page"),
         backgroundColor: Colors.blue,
       ),
-      drawer: mainDrawer(),
-      body: Center(
+      drawer: const mainDrawer(),
+      body: Container(
+        padding: const EdgeInsets.all(15),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "We are in the Home Page now",
-              style: TextStyle(fontSize: 29),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: ()=>{
+                    Navigator.of(context).pushNamed('/login')
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.blue.withOpacity(0.3),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.white.withOpacity(0.5),
+                              spreadRadius: 5.0,
+                              blurRadius: 7,
+                              offset: Offset(0,3)
+                          ),
+                        ]
+                    ),
+                    child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.person,size: 80,color: Colors.yellow,),
+                            SizedBox(height: 10,),
+                            Text(
+                              "Solo Travel",
+                              style: TextStyle(
+                                  color: Colors.brown,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: ()=>{
+                    Navigator.of(context).pushNamed('/register')
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.green.withOpacity(0.3),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.white.withOpacity(0.5),
+                              spreadRadius: 5.0,
+                              blurRadius: 7,
+                              offset: Offset(0,3)
+                          ),
+                        ]
+                    ),
+                    child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.event,size: 80,color: Colors.orange,),
+                            SizedBox(height: 10,),
+                            Text(
+                              "Event booking",
+                              style: TextStyle(
+                                  color: Colors.brown,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ),],
             ),
-            ElevatedButton(
-              child: Text('Go to details'),
-              onPressed: ()=>{
-               Navigator.of(context).pushNamed('/details')
-              },
-            ),
-            ElevatedButton(
-              child: Text(
-                "Go to login",
-              ),
-              onPressed: ()=>{
-                Navigator.of(context).pushNamed('/login')
-              },
+            const SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: ()=>{
+                    Navigator.of(context).pushNamed('/login')
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.orange.withOpacity(0.3),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.white.withOpacity(0.5),
+                              spreadRadius: 5.0,
+                              blurRadius: 7,
+                              offset: Offset(0,3)
+                          ),
+                        ]
+                    ),
+                    child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.school,size: 80,color: Colors.teal,),
+                            SizedBox(height: 10,),
+                            Text(
+                              "Institution booking",
+                              style: TextStyle(
+                                  color: Colors.brown,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: ()=>{
+                    Navigator.of(context).pushNamed('/register')
+                  },
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.purple.withOpacity(0.5),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.white.withOpacity(0.5),
+                              spreadRadius: 5.0,
+                              blurRadius: 7,
+                              offset: Offset(0,3)
+                          ),
+                        ]
+                    ),
+                    child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.local_shipping,size: 80,color: Colors.green.withOpacity(0.6),),
+                            SizedBox(height: 10,),
+                            Text(
+                              "Percel cending",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold
+                              ),
+                            )
+                          ],
+                        )
+                    ),
+                  ),
+                ),],
             )
           ],
         ),
-      ),
+      )
     );
   }
 }
